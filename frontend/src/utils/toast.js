61f3ed38ea99b053edc33abbc8f49649cced4fdf
Gurveer Singh <sgurveer97@gmail.com>
2026-03-06 14:36:51 +0530

    feat: Add Axios instance with JWT token auto-attachment and toast notification utilities.

---
+++ frontend/src/utils/toast.js
@@ -0,0 +1,38 @@
+import { Flip, toast } from "react-toastify";
+
+export const showSuccess = (message) =>
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Flip,
+    });
+export const showError = (message) =>
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Flip,
+    });
+export const showInfo = (message) =>
+    toast.info(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Flip,
+    });
