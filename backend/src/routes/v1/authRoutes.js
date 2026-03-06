80d0ecbc42d7e5ff3a288d637fcc43c3ebdc8312
Gurveer Singh <sgurveer97@gmail.com>
2026-03-06 14:16:42 +0530

    feat: Add and apply rate limiting to the `/register` and `/login` authentication routes.

--- backend/src/routes/v1/authRoutes.js
+++ backend/src/routes/v1/authRoutes.js
@@ -1,14 +1,24 @@
 import express from "express";
 import { Router  } from "express";
+import rateLimit from "express-rate-limit";
 import { register, login, logout, getProfile } from "../../controllers/authController.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 import validate from "../../middlewares/validate.js";
 import { registerSchema, loginSchema } from "../../validators/authValidator.js";
 
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //minutes in milliseconds
+    max: 2, //max attempts(from same IP)
+    message: { success: false, message: "Too many attempts, please try again after 15 minutes" },
+    standardHeaders: true,   // sends RateLimit headers in response
+    legacyHeaders: false, //old header format disabled
+});
+
+
 const router = Router();
 
-router.post("/register", validate(registerSchema), register);
-router.post("/login", validate(loginSchema), login);
+router.post("/register",authLimiter, validate(registerSchema), register);
+router.post("/login", authLimiter, validate(loginSchema), login);
 router.post("/logout", logout);
 router.get("/profile", verifyToken, getProfile);  // protected
 
